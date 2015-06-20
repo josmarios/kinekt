3fb4eb337ea2dbd08c63408508f853d7e71a386f
shcsr <shcsr@uwm.edu>
2015-06-20 16:34:50 -0500

    Update on map_search.

--- app/controllers/map_search.js
+++ app/controllers/map_search.js
@@ -5,28 +5,65 @@
  * @uses core
  */
 var APP = require("core");
-
 var CONFIG = arguments[0];
-
 APP.log("debug", "home | " + JSON.stringify(CONFIG));
+
+var locations = [{
+	"lat": 41.8911911,
+	"longi": -87.6324906
+}, {
+	"lat": 41.8916184,
+	"longi": -87.6330217
+}, {
+	"lat": 41.8916464,
+	"longi": -87.6311066
+}, {
+	"lat": 41.8924251,
+	"longi": -87.6329734
+}];
+
+//FUNCTIONS
+var loadLocations = function() {
+	for(var i = 0; i < 4; i++) {
+		var location = Alloy.Globals.Map.createAnnotation({
+			title: "Location " + i,
+			latitude: locations[i].lat,
+			longitude: locations[i].longi,
+			pincolor: 88
+		});
+		//Ti.API.info(location.title);
+
+		$.map.addAnnotation(location);
+	}
+};
 
 //INIT
 var init = function() {
 
-	var location = Alloy.Globals.Map.createAnnotation({
-		title: "Your Location",
-		latitude: 43.0782630,
-		longitude: -87.8819690
+	Ti.Geolocation.getCurrentPosition(function(e) {
+		Ti.API.info(e.coords);
+		var location = Alloy.Globals.Map.createAnnotation({
+			title: "Your Location",
+			latitude: 41.8906600,
+			longitude: 41.8906600
+			//latitude: e.coords.latitude,
+			//longitude: e.coords.longitude
+		});
+
+		$.map.addAnnotation(location);
+
+		$.map.region = {
+			latitude: 41.8906600,
+			longitude: 41.8906600,
+			// latitude: e.coords.latitude,
+			// longitude: e.coords.longitude,
+			latitudeDelta: 0.05,
+			longitudeDelta: 0.05
+		};
+
 	});
 
-	$.map.addAnnotation(location);
-
-	$.map.region = {
-		latitude: 43.0782630,
-		longitude: -87.8819690,
-		latitudeDelta: 0.05,
-		longitudeDelta: 0.05
-	};
+	loadLocations();
 
 	$.NavigationBar.setBackgroundColor(APP.Settings.colors.primary);
 
@@ -34,19 +71,30 @@ var init = function() {
 		$.NavigationBar.showBack(function(_event) {
 			APP.removeChild();
 		});
-	}
-
-	if(APP.Settings.useSlideMenu) {
-		$.NavigationBar.showMenu(function(_event) {
-			APP.toggleMenu();
-		});
 	} else {
-		$.NavigationBar.showSettings(function(_event) {
-			APP.openSettings();
-		});
+		if(APP.Settings.useSlideMenu) {
+			$.NavigationBar.showMenu(function(_event) {
+				APP.toggleMenu();
+			});
+		} else {
+			$.NavigationBar.showSettings(function(_event) {
+				APP.openSettings();
+			});
+		}
 	}
 
 };
+
+//LISTENERS
+$.map.addEventListener('click', function(e) {
+	Ti.API.info(e.annotation);
+	alert(e.title);
+	APP.addChild("place_detail", {
+		isChild: true,
+		heading: e.title,
+		text: "More info here",
+	});
+});
 
 //BOOTSTRAP
 init();
